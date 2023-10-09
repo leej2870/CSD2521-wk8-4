83a9615c4eca4f760d690eb58c900a5ceb34bcbd
leej2870 <142951387+leej2870@users.noreply.github.com>
2023-10-09 16:41:05 -0400

    Update HelloTest1.java
    Updated header comments to include update time. Changed line 9 comments.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on: 10/9/2023 by JL
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull request
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
